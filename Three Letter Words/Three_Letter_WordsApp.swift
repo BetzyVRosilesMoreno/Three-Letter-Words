19660de1bebd75689d80e397ec5bf66481f3bcd9
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-11-13 09:31:19 -0600

    Initial Commit

---
+++ Three Letter Words/Three_Letter_WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordsApp.swift
+//  Three Letter Words
+//
+//  Created by Betzy Moreno on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
